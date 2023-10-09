b4f27017db880e5edd51a459fd39aa6400694920
itsmesakthi <129639908+itsmesakthi@users.noreply.github.com>
2023-10-09 20:27:02 +0530

    Update 09-10-2023
    Update 09-10-2023

---
+++ Cars/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Cars.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public static List<string> Names=new List<string>() { "tata nano","maruti suzuki","toyota fortuner" };
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Names;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return Names[id];
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            Names.Add(value);
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            Names[id] = value;
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Names.Remove(Names[id]);
+        }
+    }
+}
